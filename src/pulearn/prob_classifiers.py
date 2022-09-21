ac12a41e558aca8c1f09406e18d83d76b777dae6
Nikos Kyriakakis <nikoskyriakakis1997@outlook.com.gr>
2022-09-21 16:41:20 +0300

    Created separate package for pu-learning utilities

--- src/pulearn/prob_classifiers.py
+++ src/pulearn/prob_classifiers.py
@@ -1,18 +1,6 @@
 import numpy as np
 
-from abc import ABC, abstractmethod
-
-class PUClassifier(ABC):
-    def __init__(self) -> None:
-        """ Base PU learning class """
-
-    @abstractmethod
-    def fit(self, X, y):
-        """ Override to specify training behaviour """
-
-    @abstractmethod
-    def predict(self, X):
-        """ Override to specify testing behaviour """
+from pubase import PUClassifier
 
 
 class ENClassifier(PUClassifier):
