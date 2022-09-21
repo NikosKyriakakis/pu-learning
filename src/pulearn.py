02f7a3f355cecf1562b16b745463e20b3a9f4163
Nikos Kyriakakis <nikoskyriakakis1997@outlook.com.gr>
2022-09-21 13:24:26 +0300

    Major code structure refactoring

--- src/pulearn.py
+++ src/pulearn.py
@@ -2,11 +2,24 @@ import numpy as np
 
 from abc import ABC, abstractmethod
 
+class PUClassifier(ABC):
+    def __init__(self) -> None:
+        """ Base PU learning class """
 
-class PULearner(ABC):
+    @abstractmethod
+    def fit(self, X, y):
+        """ Override to specify training behaviour """
+
+    @abstractmethod
+    def predict(self, X):
+        """ Override to specify testing behaviour """
+
+
+class ENClassifier(PUClassifier):
+    """ 
+        Classifier based on the first iteration of the Elkan Noto pape
     """
-        A base class which embeddes the core PU learning functionality
-    """
+
     def __init__(self, estimator, ratio=0.1) -> None:
         """ Constructor
 
@@ -14,10 +27,13 @@ class PULearner(ABC):
             estimator (sklearn.BaseEstimator): the model to use for the classification
             ratio (float, optional): the precentage of positive samples to leave unchanged. Defaults to 0.1.
         """
+
+        super().__init__()
+
         self.estimator = estimator
         self.ratio = ratio
         self.Ps1y1 = 0.0
-
+    
     @property
     def estimator(self):
         return self._estimator
@@ -50,68 +66,8 @@ class PULearner(ABC):
         if value < 0.0 or value > 1.0:
             raise ValueError("[o_O] Probability values should be between 0-1")
         self._Ps1y1 = value
-
-    @abstractmethod
-    def extract_pos_sample(self, target):
-        """ 
-            Abstract method which when instantiated extracts 
-            a positive sample based on some algorithm
-        """
-
-    def fit(self, X, y):
-        """ Fit the classifier on the data
-
-        Args:
-            X (pandas.DataFrame): the data without any labels
-            y (pandas.Series): the pu labels
-
-        Returns:
-            numpy float: the probability that a positive labeled example is actually positive
-        """
-        
-
-        # Extract positive sample
-        indices = self.extract_pos_sample(y)
-        X_out = X.iloc[indices, :]
-
-        # Remove drawn sample
-        X = X.drop(indices)
-        y = y.drop(indices)
-
-        # Fit the model to learn 
-        # the probability that an 
-        # element is labeled Pr(s=1|x)
-        self.estimator.fit(X, y)
-
-        # Predict the probability that the known positive samples are labeled
-        predictions = self.estimator.predict_proba(X_out)[:, 1]    
-        
-        # Calculate the mean probability of the above predictions --> Pr(s=1|y=1)
-        self.Ps1y1 = np.mean(predictions)
-
-    def pu_prob(self, X):
-        predicted_probs = self.estimator.predict_proba(X)[:, 1]
-
-        try:
-            predicted_probs /= self.Ps1y1
-        except ZeroDivisionError:
-            print("[o_O] Division by zero in pu_prob method --> Pr(s=1|y=1) = {}".format(self.Ps1y1))
-            predicted_probs = 0
-
-        return predicted_probs 
-
-
-class SCARLearner(PULearner):
-    """PU classifier which utilizes the Selected Completely at Random labeling mechanism
-
-    Args:
-        PULearner (object): the base PU classifier
-    """
-
-    def __init__(self, estimator, ratio=0.1) -> None:
-        super().__init__(estimator, ratio)
-
-    def extract_pos_sample(self, target):
+            
+    def extract_sample(self, target):
         """ Select positive examples completely at random 
 
         Args:
@@ -131,3 +87,55 @@ class SCARLearner(PULearner):
         sample = positive_indices[:threshold]
 
         return sample
+
+    def fit(self, X, y):
+        """ Fit the classifier on the data
+
+        Args:
+            X (pandas.DataFrame): the data without any labels
+            y (pandas.Series): the pu labels
+
+        Returns:
+            numpy float: the probability that a positive labeled example is actually positive
+        """
+
+        # Extract positive sample
+        indices = self.extract_sample(y)
+        X_out = X.iloc[indices, :]
+
+        # Remove drawn sample
+        X = X.drop(indices)
+        y = y.drop(indices)
+
+        # Fit the model to learn 
+        # the probability that an 
+        # element is labeled Pr(s=1|x)
+        self.estimator.fit(X, y)
+
+        # Predict the probability that the known positive samples are labeled
+        predictions = self.estimator.predict_proba(X_out)[:, 1]    
+        
+        # Calculate the mean probability of the above predictions --> Pr(s=1|y=1)
+        self.Ps1y1 = np.mean(predictions)
+
+    def predict(self, X, threshold=0.5):
+        """ Make the actual predictions
+
+        Args:
+            X (pandas.DataFrame): the data without labels
+            threshold (float, optional): a bound to decide when an example is positive or negative. Defaults to 0.5.
+
+        Returns:
+            list: the final predictions
+        """
+        predicted_probs = self.estimator.predict_proba(X)[:, 1]
+
+        try:
+            predicted_probs /= self.Ps1y1
+        except ZeroDivisionError:
+            print("[o_O] Division by zero in pu_prob method --> Pr(s=1|y=1) = {}".format(self.Ps1y1))
+            predicted_probs = 0
+
+        predictions = [1 if prob > threshold else 0 for prob in predicted_probs] 
+
+        return predictions
