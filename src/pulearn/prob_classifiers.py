79791c2ed9fcfd5ee8929e49f11dc7fe040f69e2
Nikos Kyriakakis <nikoskyriakakis1997@outlook.com.gr>
2022-09-22 09:48:27 +0300

    Separated PU learning classes

--- src/pulearn/prob_classifiers.py
+++ src/pulearn/prob_classifiers.py
@@ -1,6 +1,6 @@
-import numpy as np
+from pulearn.pubase import *
 
-from pubase import PUClassifier
+import numpy as np
 
 
 class ENClassifier(PUClassifier):
@@ -8,7 +8,7 @@ class ENClassifier(PUClassifier):
         Classifier based on the first iteration of the Elkan Noto pape
     """
 
-    def __init__(self, estimator, ratio=0.1) -> None:
+    def __init__(self, estimator) -> None:
         """ Constructor
 
         Args:
@@ -19,7 +19,6 @@ class ENClassifier(PUClassifier):
         super().__init__()
 
         self.estimator = estimator
-        self.ratio = ratio
         self.Ps1y1 = 0.0
     
     @property
@@ -31,21 +30,6 @@ class ENClassifier(PUClassifier):
         self._estimator = value
 
     @property
-    def ratio(self):
-        return self._ratio
-
-    @ratio.setter
-    def ratio(self, value):
-        if type(value) != float:
-            self._ratio = 0.1
-            return
-
-        if value < 0.0 or value > 1.0:
-            self._ratio = 0.1
-        else:
-            self._ratio = value
-
-    @property
     def Ps1y1(self):
         return self._Ps1y1
 
@@ -54,29 +38,8 @@ class ENClassifier(PUClassifier):
         if value < 0.0 or value > 1.0:
             raise ValueError("[o_O] Probability values should be between 0-1")
         self._Ps1y1 = value
-            
-    def extract_sample(self, target):
-        """ Select positive examples completely at random 
 
-        Args:
-            target (array like): the target values
-            ratio (float, optional): the percentage of positive examples to keep. Defaults to 0.25.
-
-        Returns:
-            array like: a subset of the positive examples
-        """
-
-        # Keep only positive indices and shuffle them
-        positive_indices = np.where(target.values == 1)[0]
-        np.random.shuffle(positive_indices)
-
-        # Cut off a specified percentage of the positive examples
-        threshold = int(np.ceil(self.ratio * len(positive_indices)))
-        sample = positive_indices[:threshold]
-
-        return sample
-
-    def fit(self, X, y):
+    def fit(self, X, y, ratio=0.2):
         """ Fit the classifier on the data
 
         Args:
@@ -88,7 +51,7 @@ class ENClassifier(PUClassifier):
         """
 
         # Extract positive sample
-        indices = self.extract_sample(y)
+        indices = extract_sample(y, ratio)
         X_out = X.iloc[indices, :]
 
         # Remove drawn sample
@@ -134,8 +97,9 @@ class WeightedENCLassifier(ENClassifier):
         The second algorithm of the Elkan Noto paper
     """
 
-    def __init__(self, estimator, labeled, unlabeled, ratio=0.1) -> None:
-        super().__init__(estimator, ratio)
+    def __init__(self, estimator, labeled, unlabeled) -> None:
+        super().__init__(estimator)
+        
         self.labeled = labeled
         self.unlabeled = unlabeled
 
--- src/pulearn/pubase.py
+++ src/pulearn/pubase.py
@@ -1,11 +1,36 @@
 from abc import ABC, abstractmethod
 
+import numpy as np
+
+
+def extract_sample(target, ratio):
+    """ Select positive examples completely at random 
+
+    Args:
+        target (array like): the target values
+        ratio (float, optional): the percentage of positive examples to keep. Defaults to 0.25.
+
+    Returns:
+        array like: a subset of the positive examples
+    """
+
+    # Keep only positive indices and shuffle them
+    positive_indices = np.where(target.values == 1)[0]
+    np.random.shuffle(positive_indices)
+
+    # Cut off a specified percentage of the positive examples
+    threshold = int(np.ceil(ratio * len(positive_indices)))
+    sample = positive_indices[:threshold]
+
+    return sample
+
+
 class PUClassifier(ABC):
-    def __init__(self) -> None:
+    def __init__(self, ratio) -> None:
         """ Base PU learning class """
 
     @abstractmethod
-    def fit(self, X, y):
+    def fit(self, X, y, ratio=0.2):
         """ Override to specify training behaviour """
 
     @abstractmethod
