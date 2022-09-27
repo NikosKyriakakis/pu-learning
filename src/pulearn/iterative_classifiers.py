cf600d34116bb289866bfd5db547b78a3acb565e
Nikos Kyriakakis <nikoskyriakakis1997@outlook.com.gr>
2022-09-27 21:38:57 +0300

    Refactored iterative classifier code

--- src/pulearn/iterative_classifiers.py
+++ src/pulearn/iterative_classifiers.py
@@ -8,82 +8,98 @@ import pandas as pd
 import numpy as np
 
 
-class RocSVM(PUClassifier):
-    """ Initial implementation based on the paper 
-            'Learning to Classify Texts Using Positive and Unlabeled Data'
-    """
-    def __init__(self) -> None:
+def estimator_factory(option, params):
+    models = {
+        "Rocchio": (lambda: NearestCentroid(**params)),
+        "SVC": (lambda: SVC(**params))
+    }
+
+    if option in models.keys():
+        estimator = models[option]
+    else:
+        estimator = None
+    return estimator
+
+
+class TwostepClassifier(PUClassifier):
+    def __init__(self, est_name1, est_name2, params1={}, params2={}) -> None:
         super().__init__()
-        self._best_model = None
+        self._estimator = None
+        self.est_name1 = est_name1
+        self.est_name2 = est_name2
+        self.params1 = params1
+        self.params2 = params2
+
+
+    def isolate_reliable_sample(self, Ux, Uy, value):
+        # Use the estimator on the unlabeled set
+        predictions = self._estimator.predict(Ux)
+        # Extract negative prediction indices
+        indices = np.where(predictions == value)[0]
+        if indices.size == 0:
+            return None
+        else:
+            # Select the samples based on the indices
+            Qx = Ux.iloc[indices, :]
+            Qy = Uy[indices]
+            # Remove extracted negatives from unlabeled data
+            Ux = Ux.drop(indices)
+            Uy = Uy.drop(indices)
+            Ux = Ux.reset_index(drop=True)
+            Uy = Uy.reset_index(drop=True)
+
+            return Ux, Uy, Qx, Qy
 
 
     def fit(self, X, y):
-        # Separate the data into Positive & Unlabeled sets
-        Px, Py, Ux, Uy = separate_sets(X, y)
-        Ux_test = Ux.copy()
+        # Separate positive & unlabeled sets
+        Px, \
+        Py, \
+        Ux, \
+        Uy = separate_sets(X, y)
 
-        # Create initial Rocchio classifier
-        rocchio = NearestCentroid()
-        rocchio.fit(X, y)
+        # Create a copy to retain a separate 
+        # test set when the model has been fitted
+        U_test = Ux.copy()
 
-        # Create the U^L set (i.e. from this set we will extract reliably negative examples)
-        UL = rocchio.predict(Ux)
+        # Create initial model & fit on the whole data (P+U)
+        self._estimator = estimator_factory(self.est_name1, self.params1)()
+        self._estimator.fit(X, y)
 
-        # Extract reliably negative samples
-        indices = np.where(UL == 0)[0]
-        Qx = Ux.iloc[indices, :]
-        Qy = Uy[indices]
+        Ux, Uy, Qx, Qy = self.isolate_reliable_sample(Ux, Uy, 0)
 
-        # Create a copy of the Reliably Negatives
         RNx = Qx.copy()
         RNy = Qy.copy()
 
-        # Remove extracted negatives from unlabeled data
-        Ux = Ux.drop(indices)
-        Uy = Uy.drop(indices)
-        Ux = Ux.reset_index(drop=True)
-        Uy = Uy.reset_index(drop=True)
-
-        # We want to retain a copy of the original positives
+        # We want to retain a copy of 
+        # the original positives
         Dx = Px.copy()
 
         count = 0
         while True:
             # Initialize the model
-            svc = SVC()
+            self._estimator = estimator_factory(self.est_name2, self.params2)()
 
-            # Use positives and reliable negatives as input to the SVM
+            # Use positives and reliable negatives as input to the actual classifier
             Dx = pd.concat([Dx, RNx], ignore_index=True)
             Py = pd.concat([Py, RNy], ignore_index=True)
-            svc.fit(Dx, Py)
+            self._estimator.fit(Dx, Py)
 
             # Save the first iteration
             # in case we need to rollback 
             # to this version
             if count == 0:
-                self._best_model = deepcopy(svc)
+                self._best_model = deepcopy(self._estimator)
 
             print("Iteration: {}".format(count))
             print("Unlabeled points remaining: {}\n".format(Ux.shape[0]))
             count += 1
                 
-            # Classify the rest of the unlabeled
-            # points to extract more reliable negatives
-            UL = svc.predict(Ux)
-            indices = np.where(UL == 0)[0]
-            # If no negative predictions are found,
-            # we stop the loop
-            if indices.size == 0:
+            values = self.isolate_reliable_sample(Ux, Uy, 0)
+            if values is None:
                 break
-            else:
-                # Extract new reliable negatives
-                Qx = Ux.iloc[indices, :]
-                Qy = Uy[indices]
-                # Delete extracted items from the unlabeled set
-                Ux = Ux.drop(indices)
-                Uy = Uy.drop(indices)
-                Ux = Ux.reset_index(drop=True)
-                Uy = Uy.reset_index(drop=True)
+            
+            Ux, Uy, Qx, Qy = values
 
             if Ux.empty:
                 break
@@ -95,19 +111,24 @@ class RocSVM(PUClassifier):
 
         # Test the final classifier on the 
         # initial positive set
-        predictions = svc.predict(Px)
+        predictions = self._estimator.predict(Px)
         positive = len([x for x in predictions if x == 1])
         negative = len([x for x in predictions if x == 0])
 
-        ratio = negative / positive
-        print("Negative/Positive ratio: {}".format(ratio))
+        try:
+            ratio = negative / positive
+            print("Negative/Positive ratio: {}".format(ratio))
+        except ZeroDivisionError:
+            # Dummy value
+            ratio = 0.1
+
         # If the misclassified points are above 5% 
         # we will rollback to the cached model
         # Else we keep the final classifier
         if ratio < 0.05:
-            self._best_model = svc   
+            self._best_model = self._estimator   
 
-        return Ux_test
+        return U_test
 
     def predict(self, X):
         # Make a prediction on the whole unlabeled set
