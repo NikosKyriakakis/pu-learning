e1de76f1f58a8c3ba3d82a83a923162edfa510cc
Nikolaos Kyriakakis <nikoskyriakakis1997@outlook.com.gr>
2022-12-23 21:01:48 +0200

    Added lstm classifier and removed mlp

--- src/pulearn/neural_nets/estimators.py
+++ src/pulearn/neural_nets/estimators.py
@@ -3,8 +3,9 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as F
-import torchmetrics as tm
 import numpy as np
+
+from console import *
 
 from pulearn\
     .neural_nets\
@@ -12,55 +13,94 @@ from pulearn\
     .nnpu_loss import *
 
 
-class MLP5(nn.Module):
-    def __init__(self, pretrained_embedding, max_len, kernel_size=8) -> None:
+class NeuralNetEstimator(nn.Module):
+    def __init__(self, pretrained_embeddings, vocab_len, embed_dim, num_classes=1) -> None:
         super().__init__()
 
-        self.embedding = nn.Embedding.from_pretrained (
-            pretrained_embedding
-        )
+        self.num_classes = num_classes
 
-        embed_size = pretrained_embedding.shape[1]
-        input_size = int((embed_size * max_len) / kernel_size)
-        self.input_layer = nn.Linear(input_size, 300)
-        self.hidden_layer = nn.Linear(300, 300)
-        self.last_layer = nn.Linear(300, 2)
-        self.dropout = nn.Dropout()
-        self.softmax = nn.Softmax(dim=-1)
+        if pretrained_embeddings is None \
+            and type(vocab_len) == int \
+            and type(embed_dim) == int:
+            self.embedding_layer = nn.Embedding(num_embeddings=vocab_len, embedding_dim=embed_dim)
+            self.embed_dim = embed_dim
+            self.vocab_len = vocab_len
+        else:
+            self.embedding_layer = nn.Embedding.from_pretrained(pretrained_embeddings)
+            self.vocab_len = pretrained_embeddings.shape[0]
+            self.embed_dim = pretrained_embeddings.shape[1]
 
-    def forward(self, input_ids):
-        X = self.embedding(input_ids).float()
-        X = X.view(X.shape[0], -1)
-        X = F.avg_pool1d(X, kernel_size=8) 
-        X = F.softsign(self.input_layer(X))
-        X = self.dropout(X)
-        X = self.softmax(self.last_layer(X))
-        
-        return X
+    def forward(self, X):
+        raise NotImplementedError(error("Abstract method called"))
 
 
-class CNNEstimator(nn.Module):
+class LSTM_Estimator(NeuralNetEstimator):
     def __init__(
         self, 
-        num_classes, 
-        pretrained_embedding, 
+        pretrained_embeddings, 
+        vocab_len=None, 
+        embed_dim=None,
+        num_layers=1,
+        hidden_dim=50,
+        num_classes=1
+    ) -> None:
+
+        super().__init__(pretrained_embeddings, vocab_len, embed_dim, num_classes)
+        
+        self.lstm = nn.LSTM (
+            input_size=self.embed_dim, 
+            hidden_size=hidden_dim,
+            num_layers=num_layers, 
+            batch_first=True
+        ) 
+
+        self.num_layers = num_layers
+        self.hidden_size = hidden_dim
+        self.linear_in = nn.Linear(hidden_dim, 128) 
+        self.linear_out = nn.Linear(128, num_classes)
+
+        self.relu = nn.ReLU()
+
+    def forward(self, X):
+        X = self.embedding_layer(X).float()
+        
+        # Calculate initial hidden & internal states
+        h_0 = torch.zeros(self.num_layers, X.size(0), self.hidden_size)
+        c_0 = torch.zeros(self.num_layers, X.size(0), self.hidden_size)
+
+        # Propagate input through LSTM
+        out, _ = self.lstm(X, (h_0, c_0)) 
+
+        # Reshape input
+        out = out[:, -1, :]
+
+        # Forward input through 
+        # the fully connected layers
+        out = self.linear_in(out)
+        out = self.relu(out)
+        out = self.linear_out(out)
+
+        return out
+
+
+class CNN_Estimator(NeuralNetEstimator):
+    def __init__(
+        self,  
+        pretrained_embeddings, 
+        vocab_len=None,
+        embed_dim=None,
         num_filters=[100, 100, 100], 
         filter_sizes=[3, 4, 5],
         dropout=0.5,
-        apply_softmax=False
+        apply_softmax=False,
+        num_classes=1,
     ) -> None:
 
-        super().__init__()
-
-        self.embedding = nn.Embedding.from_pretrained (
-            pretrained_embedding
-        )
-
-        dim = pretrained_embedding.shape[1]
+        super().__init__(pretrained_embeddings, vocab_len, embed_dim, num_classes)
 
         self.convolution_modules = nn.ModuleList ([
             nn.Conv1d(
-                in_channels=dim, 
+                in_channels=self.embed_dim, 
                 out_channels=num_filters[i], 
                 kernel_size=filter_sizes[i]
             ) for i in range(len(filter_sizes))
@@ -72,7 +112,7 @@ class CNNEstimator(nn.Module):
         self.softmax = nn.Softmax(dim=-1)
 
     def forward(self, input_ids):
-        X = self.embedding(input_ids).float()
+        X = self.embedding_layer(input_ids).float()
         X = X.permute(0, 2, 1)
         X = [F.relu(conv_module(X)) for conv_module in self.convolution_modules]
         X = [F.max_pool1d(x_i, kernel_size=x_i.shape[2]) for x_i in X]
@@ -96,7 +136,6 @@ class PUNet(pl.LightningModule):
 
         self.learning_rate = learning_rate
         self.estimator = estimator
-        # self.f1_score = tm.F1Score(average="macro", num_classes=1)
 
     def forward(self, X_in):
         X_out = self.estimator(X_in)
@@ -121,9 +160,7 @@ class PUNet(pl.LightningModule):
         labels = labels.type(torch.float)
 
         loss = self.loss(logits.view(-1), labels)
-        # self.f1_score(logits.view(-1), labels.type(torch.int64))
         self.log(f"{stage}_loss", loss, on_epoch=True, prog_bar=True)
-        # self.log(f"{stage}_F1", self.f1_score, on_epoch=True, prog_bar=True)
 
         return loss
 
