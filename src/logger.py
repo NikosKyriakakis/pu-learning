dc1cf8115bb6c640516793bb20ba4b5a0100a879
Nikolaos Kyriakakis <nikoskyriakakis1997@outlook.com.gr>
2023-07-13 18:42:40 +0300

    - Organized imports - Updated requirements.txt - Updated .gitignore - Removed unused function from logger

--- src/logger.py
+++ src/logger.py
@@ -6,8 +6,3 @@ def save_logs(output):
     with open("report.log", "a") as checkpoint:
         json.dump(output, checkpoint, indent=4)
         checkpoint.write(",\n")
-
-
-def reset_logging(output="EXPERIMENT START"):
-    with open("report.log", "a") as checkpoint:
-        checkpoint.write("\n" + 45 * "=" + " " + output + " " + 45 * "=" + "\n")
